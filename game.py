48c530094026e3e80858495d8aa88e9a839bea8d
Gabriel Loring <gloring@csumb.edu>
2017-11-25 15:43:33 -0800

    Minor changes to names
    Update room names to reflect story.
    Moved some rooms around to be consistent across descriptions

--- game.py
+++ game.py
@@ -43,14 +43,16 @@ MAP_WIDTH = 3
 EMPTY_ENTERS_TO_BAIL_OUT = 3
 
 # A global dictionary to hold player attributes
-player = { "health" :  100, 
-           "location": "start",    
-           "inventory": "nothing"}
+player = { 
+    "health" :  100, 
+    "location": "start",    
+    "inventory": "nothing"}
 
 # Lab 12 item C requires a loose condition a Non player character may be useful
-larry = { "health" :  100, 
-          "location": "start",
-          "inventory": "nothing"}
+larry = { 
+    "health" :  100, 
+    "location": "start",
+    "inventory": "nothing"}
 
 
 # OK apologies, the map is stored in triplicate!
@@ -66,70 +68,76 @@ larry = { "health" :  100,
 # The third copy of the map is in the rooms dictionary.  This lets me look 
 # up a specific rooms dictionary using its name.  not sure how happy I am with
 # this, it is super hacky
-map = [ [ "office", "lobby", "hall"],
-        [ "hidden", "street", "safe"],
-        [ "park", "start", "ally"]]
+map = [ [ "office",        "staircase", "hidden"],
+        [ "billiards",     "ballroom",  "library"],
+        [ "park",          "start",     "getaway_vehicle"]]
 
-office = { "room_description" : "The room is XYZ, to the north is XYZ, to the east lies XYZ the south wall is blocked and to the east is y",
-           "passable_NESW" : "NYYN",
-           "npc" : larry,
-           "object" : "nothing"}  
+office = { 
+    "room_description" : "The the office is a nice wood paneled room with protraits of stuffy old dudeds on the North, West and South walls.  There is a desk with a key on it labled 'Library_key'",
+    "passable_NESW" : "NYNN",
+    "npc" : larry,
+    "object" : "library_key"}  
 
-library = {
-    "room_description": "The room is the library. The library holds books to the north, east, and west. Along these walls is a special book that leads to a hidden room. Find the book that hisses and slither and you will find the secret room.",
-    "passable_NESW": "NYYY",
-    "npc": larry,
-    "object": "nothing"}
-
-ballroom = {
-    "room_description": "You have entered the ballroom. The ballroom is filled with 100 guests, all dancing. To the north is the staircase, to the east lies the library, to the west lies the billiards room. You must blend in with the party. Do not get caught as you make your way to the next room.",
-    "passable_NESW": "NNCY",
-    "npc": larry,
-    "object": "nothing"}
+staircase = { 
+    "room_description" : "The saircase is ornaite and fancey designed to allow someone to make a grand entrance.  The stairs start on the south and exit to the west on the top floor.  Trying to exit to the north or east would result in a nasty fall",
+    "passable_NESW" : "NNYY",
+    "npc" : larry,
+    "object" : "nothing"}   
 
 hidden = {
     "room_description": "You've found the hidden room. Here lies the Catalina necklace in a locked wooden chest. To open the chest you must first unlock the secret ",
-    "passable_NESW": "YNNN",
+    "passable_NESW": "NNYN",
+    "npc": larry,
+    "object": "necklace"}
+
+billiards = {
+    "room_description": "This is the billiards room. To the north, east and south walls are blocked off.To exit return east. To open the vault type open.",
+    "passable_NESW": "NYNN",
+    "npc": larry,
+    "object": "secret_book"}
+
+ballroom = {
+    "room_description": "You have entered the ballroom. The ballroom is filled with 100 guests, all dancing. To the north is the staircase, to the east lies the library, to the west lies the billiards room. You must blend in with the party. Do not get caught as you make your way to the next room.",
+    "passable_NESW": "YCYY",
+    "npc": larry,
+    "object": "nothing"}
+
+library = {
+    "room_description": "The room is the library. The library holds books to the north, south, and west. Along these walls is a special book that leads to a hidden room. Find the book that hisses and slither and you will find the secret room.",
+    "passable_NESW": "CNNY",
     "npc": larry,
     "object": "nothing"}
 
 
-street = { "room_description" : "The room is XYZ, to the north is XYZ, to the east lies XYZ the south wall is blocked and to the east is y",
-           "passable_NESW" : "YNYN",
-           "npc" : larry,
-           "object" : "nothing"}   
+park = {   
+    "room_description" : "The park is nice, a good place to take your dog, or to step in something left by somonelses dog. To the north is the back wall of the museum, east leads to the starting position and both south and west lead to the the edge of the game world",
+    "passable_NESW" : "NYNN",
+    "npc" : larry,
+    "object" : "nothing"}  
 
-safe = {
-    "room_description": "This is the safe vault room. To the north lies the vault. The east wall is blocked off, the west wall is blocked off. To exit return south. To open the vault type open.",
-    "passable_NESW": "YNNN",
-    "npc": larry,
-    "object": "nothing"}
+start = {  
+    "room_description" : "This is the staging area for your heist to the north is the ballroom, west is the park and south is the edge of the game world, your getaway vehicle is to the east.",
+    "passable_NESW" : "YYNY",
+    "npc" : larry,
+    "object" : "nothing"}  
 
-park = {   "room_description" : "The park is nice, a good place to take your dog, or to step in something left by somonelses dog. To the north is the back wall of the museum, east leads to the starting position and both south and west lead to the the edge of the game world",
-           "passable_NESW" : "NYNN",
-           "npc" : larry,
-           "object" : "nothing"}  
-
-start = {  "room_description" : "This is the staging area for your heist  to the north is a street, wide but with no visible traffic off to the east is an ally, west is the park and south is the edge of the game world",
-           "passable_NESW" : "YYYN",
-           "npc" : larry,
-           "object" : "nothing"}  
-
-ally = {    "room_description" : "An alley, bad things happen here",
-            "passable_NESW" : "NNNY",
-            "npc" : larry,
-            "object" : "nothing"}  
+getaway_vehicle = {    
+    "room_description" : "A 1973 Oldsmobile Delta 88, I wonder if Sam Raimi is directing this heist ",
+    "passable_NESW" : "NNNY",
+    "npc" : larry,
+    "object" : "nothing"}  
 
 
-rooms = {"office": office,
-         "library": library,
-         "ballroom": ballroom,
-         "hidden": hidden,
-         "street": street,
-         "safe": safe,
-         "park": park,
-         "start": start,
-         "ally": ally}
+rooms = {
+    "office": office,
+    "staircase": staircase,
+    "hidden": hidden,
+    "billiards": billiards,
+    "ballroom": ballroom,
+    "library": library,
+    "park": park,
+    "start": start,
+    "getaway_vehicle": getaway_vehicle}
 
 # We need lists of words for the parser
 #
@@ -144,10 +152,10 @@ rooms = {"office": office,
 #    3. Administrative 
 #        A) administrative = these are here for the program to see running program state and for the player to quit.
 travel    = [ "go", "walk", "run", "skip", "get", "move", "travel", "head" ]
-actions   = ["get", "take", "use", "open", "close", "examine", "look", "close", "show", "kick"]
+actions   = ["get", "take", "steal", "lift", "snatch", "borrow", "use", "open", "close", "examine", "look", "close", "show", "kick"]
 
 directions = [ "left", "right", "forward", "ahead", "back", "up", "down", "north", "south", "east", "west" ]
-items   = ["door", "gun", "safe", "map"]
+items   = ["door", "gun", "safe", "map", "necklace", "key", "secret" ]
 people = ["self", "dog", "scooby", "larry"]
 
 administrative = ["draw", "debug", "save", "help", "explain", "tutorial", "exit", "quit"]
@@ -155,48 +163,47 @@ administrative = ["draw", "debug", "save", "help", "explain", "tutorial", "exit"
 
   
 def debugLog(functionName, action, message):
-    print ("%s\n%04d\t%s:\t%s:\t%s"%("", 0, functionName, action, message))    
-    return
+    pass
     if functionName == "debugLog":
-        print(debugLog.string)
+        printNow(debugLog.string)
     #debugLog.counter +=1
     #debugLog.string = ("%s\n%04d\t%s:\t%s:\t%s"%(debugLog.string, debugLog.counter, functionName, action, message))
 
 def titleMessage():
-    print(title)
+    printNow(title)
     return
 
 def welcomeMessage():
-    #print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    #print(title)
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    print("Welcome to The Heist")
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    print("Type start to begin")
-    print("Type help to learn how to play")
-    print("Type exit to leave the game")
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    #printNow("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    #printNow(title)
+    printNow("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    printNow("Welcome to The Heist")
+    printNow("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    printNow("Type start to begin")
+    printNow("Type help to learn how to play")
+    printNow("Type exit to leave the game")
+    printNow("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     return
 
 def story():
-    print("Larry is on a mission to find the Catalina necklace (worth 10 million).")
-    print("He has studied the blueprints to the PyCharm Mansion.")
-    print("A ball is being thrown at the PyCharm Mansion tonight.")
-    print("Larry is entering the ball under the alias Jonathon Windsor")
-    print("You must help Larry find the Catalina necklace, but be careful not to get caught.")
-    print("As you go through the mansion you will find clues that lead you to a room with a hidden passage")
-    print("Find that passage and you will find the necklace")
+    printNow("Larry is on a mission to find the Catalina necklace (worth 10 million).")
+    printNow("He has studied the blueprints to the PyCharm Mansion.")
+    printNow("A ball is being thrown at the PyCharm Mansion tonight.")
+    printNow("Larry is entering the ball under the alias Jonathon Windsor")
+    printNow("You must help Larry find the Catalina necklace, but be careful not to get caught.")
+    printNow("As you go through the mansion you will find clues that lead you to a room with a hidden passage")
+    printNow("Find that passage and you will find the necklace")
     return   
 
 '''
 def library():
-    print("")
-    print("You have just entered the library.")
-    print("1 - North to book wall")
-    print("2 - East to Office door")
-    print("3 - West to Ballroom door")
-    print("4 - South to Street door")
-    print("Choose a direction")
+    printNow("")
+    printNow("You have just entered the library.")
+    printNow("1 - North to book wall")
+    printNow("2 - East to Office door")
+    printNow("3 - West to Ballroom door")
+    printNow("4 - South to Street door")
+    printNow("Choose a direction")
 '''
 
 def parseInput(userString):
@@ -308,19 +315,19 @@ def isAdministrative(action="", item="", subject=""):
 
 
 def tutorial():
-    print("\n\nTutorial\n\n")
-    print("The Parser uderstands actions, items and objects\n The order of words does not matter\n")
-    print("Valid commands could be \"Walk north\" or \"look at door\"\n")    
-    print("\nActions the parser understands:")
-    print(travel)  
-    print(actions)   
-    print("\nItems the parser understands:")
-    print(items)  
-    print("\nObjects the parser understands:")
-    print(people) 
-    print(directions)    
-    print("\nAdministrative commands the parser understands:")
-    print(administrative) 
+    printNow("\n\nTutorial\n\n")
+    printNow("The Parser uderstands actions, items and objects\n The order of words does not matter\n")
+    printNow("Valid commands could be \"Walk north\" or \"look at door\"\n")    
+    printNow("\nActions the parser understands:")
+    printNow(travel)  
+    printNow(actions)   
+    printNow("\nItems the parser understands:")
+    printNow(items)  
+    printNow("\nObjects the parser understands:")
+    printNow(people) 
+    printNow(directions)    
+    printNow("\nAdministrative commands the parser understands:")
+    printNow(administrative) 
 
 
 def drawMap():
@@ -332,7 +339,7 @@ def drawMap():
         canvas = canvas + "\n|\t\t|\t\t|\t\t|\n"
         canvas = canvas + "+---------------+---------------+---------------+\n"
 
-    print(canvas)    
+    printNow(canvas)    
 
 
 def isTravelCommand(action, item, subject):
@@ -360,10 +367,10 @@ def try_to_move_player_to_new_room(DIRECTION_INDEX):
         player["location"] = movePlayer(DIRECTION_INDEX)
         describeRoom()    
     elif passable == 'C':    # C is a closed door
-        print("You try but the door is closed")
+        printNow("You try but the door is closed")
         debugLog("try_to_move_player_to_new_room", "report closed door","")
     else:
-        print("You can not move in that direction")
+        printNow("You can not move in that direction")
         debugLog("try_to_move_player_to_new_room", "report could not travel","")
 
 def convert_direction_to_index(item):
@@ -442,7 +449,7 @@ def find_room_by_relative_direction(direction, currentRoomNumber):
     elif direction == WEST:
         newRoom = currentRoomNumber - 1
     else:
-        raise "Error"
+        raise "Error bad direction index"
     debugLog("find_room_by_relative_direction", "New room",newRoom)
     return newRoom
 
@@ -452,8 +459,8 @@ def describeRoom():
     '''
     currentRoom = player["location"] 
     debugLog("describeRoom", currentRoom,"")
-    print("You are in the %s"%currentRoom)   
-    print("%s"%rooms[currentRoom]["room_description"])
+    printNow("You are in the %s"%currentRoom)   
+    printNow("%s"%rooms[currentRoom]["room_description"])
 
 
 def door(action, item, subject):
@@ -468,6 +475,59 @@ def interactWithDoor(action, item, subject):
     pass 
 
 
+def office_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def staircase_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass  
+
+def hidden_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def billiards_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def ballroom_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def library_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def park_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def start_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
+
+def getaway_vehicle_handleer(action, item, subject):
+    '''
+    The room handle takes care of opening or closing doors, using inventor items, adding inventory items and NPC interactions
+    '''
+    pass 
 
 
 def gameLoop():
@@ -490,16 +550,34 @@ def gameLoop():
 
         #Are we moving
         if isTravelCommand(action, item, subject):
-            pass
             continue
-
-        # Are we interacting
-
-        # Other
 
         # Administrative commands
         if isAdministrative(action, item, subject):      
             continue
+        
+        # each room gets its own function to deal with event in it
+        if player["location"] == "office": 
+            office_handleer(action, item, subject)
+        elif player["location"] == "staircase": 
+            staircase_handleer(action, item, subject)
+        elif player["location"] == "hidden": 
+            hidden_handleer(action, item, subject)
+        elif player["location"] == "billiards": 
+            billiards_handleer(action, item, subject)
+        elif player["location"] == "ballroom": 
+            ballroom_handleer(action, item, subject)
+        elif player["location"] == "library": 
+            library_handleer(action, item, subject)
+        elif player["location"] == "park": 
+            park_handleer(action, item, subject)
+        elif player["location"] == "start": 
+            start_handleer(action, item, subject)
+        elif player["location"] == "getaway_vehicle": 
+            getaway_vehicle_handleer(action, item, subject)
+        else:
+            raise "Error no room to handle events"
+
 
     debugLog("gameLoop", "exit", ("gameOn:\t%s\tgameCycles:\t%s"%(gameOn,gameCycles)))   
 
@@ -509,3 +587,4 @@ def gameLoop():
 
 
 gameLoop()
+
